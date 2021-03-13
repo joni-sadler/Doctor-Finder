b17c2c7f7abe092afaf0b6b961bb53be04a4d501
Joni Sadler <joni.anne.sadler@gmail.com>
2021-03-13 11:23:46 -0500

    Added find all walk-in clinics functionality

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,43 @@
-const express = require("express")
-const app = express()
-const port = 8000
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 8000;
+const { addDoctor, getDoctors, getSingleDoctor } = require("./doctor-controllers");
+const { addClinic, getClinics, getSingleClinic } = require("./clinic-controllers");
+
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json()); 
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
  res.send("Backend server is working.")
 })
 
-app.listen(port, () => {
-  console.log("Listening on port 8000.")
-})
+// Get list of all clinics
+app.get("/healthcare_finder", getClinics)
+
+// Get single clinic
+app.get("/clinics/:id", getSingleClinic)
+
+// Add a clinic
+app.post("/clinic_signup", addClinic)
+
+// Get list of all doctors
+app.get("/healthcare_finder", getDoctors)
+
+// Get single doctor
+app.get("/doctors/:id", getSingleDoctor)
+
+// Add a doctor
+app.post("/doctor_signup", addDoctor)
+
+// Catch-all endpoint
+app.get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not what you are looking for.",
+    });
+  })
+
+.listen(PORT, () => console.info(`Listening on port ${PORT}`));
 
