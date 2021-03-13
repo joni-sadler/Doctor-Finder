40e636c69ecd7a900238b20c8f384943055ceae8
Joni Sadler <joni.anne.sadler@gmail.com>
2021-03-12 19:43:12 -0500

    Updated doctor and clinic controllers

--- backend/doctor-controllers.js
+++ backend/doctor-controllers.js
@@ -2,18 +2,14 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
 
+
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }
 
-// const create_clinic = (req, res) => {
-//   const { clinic } = req.body; 
-//   // add clinic to database
-//   res.status(201).json({ message, clinic });
-// }
 
-const addClinic = async (req, res) => {
+const addDoctor = async (req, res) => {
   const client = await MongoClient(MONGO_URI, options);
 
   console.log(req.body);
@@ -21,11 +17,11 @@ const addClinic = async (req, res) => {
   await client.connect();
 
   try {
-
+    
     const db = client.db("healthcare_database");
     console.log("connected!");
 
-    await db.collection("clinics").insertOne(req.body);
+    await db.collection("doctors").insertOne(req.body);
 
     res.status(201).json({ status: 201, data: req.body });
 
@@ -38,14 +34,14 @@ const addClinic = async (req, res) => {
   console.log("disconnected");
 }
 
-const getClinics = (req, res) => {
+const getDoctors = (req, res) => {
   // ingest clinic list to MongoDb and get here
 }
 
-const getSingleClinic = (req, res) => {
+const getSingleDoctor = (req, res) => {
   // get single clinic with params
   // const clinicId = req.params.id;
 }
 
 
-module.exports = { addClinic, getClinics, getSingleClinic };
+module.exports = { addDoctor, getDoctors, getSingleDoctor };
