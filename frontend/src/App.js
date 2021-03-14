f521a6aeceacad4410ff21f5cd37a6dc9a2485a4
Joni Sadler <joni.anne.sadler@gmail.com>
2021-03-14 17:00:36 -0400

    Added list of all doctors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
-import Home from "./Home";
-import About from "./About";
-import Faq from "./Faq";
-import ProviderPage from "./ProviderPage";
-import HealthcareFinder from "./HealthcareFinder";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Faq from "./pages/Faq";
+import DoctorLogin from "./pages/DoctorLogin";
+import HealthcareFinder from "./pages/HealthcareFinder";
+import DoctorSignup from "./pages/DoctorSignup";
+import ClinicSignup from "./pages/ClinicSignup";
+import WalkInClinics from "./pages/WalkInClinics";
+import AppointmentClinics from "./pages/AppointmentClinics";
+import FamilyDoctor from "./pages/FamilyDoctor";
+import Clinic from "./pages/Clinic";
+import DoctorList from "./pages/DoctorList";
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
       <Main>
         <Switch>
           <Route exact path="/">
@@ -22,11 +28,32 @@ const App = () => {
           <Route exact path="/faq">
             <Faq />
           </Route>
-          <Route exact path="/providerlogin">
-            <ProviderPage />
+          <Route exact path="/login">
+            <DoctorLogin />
           </Route>
-          <Route exact path="/healthcarefinder">
+          <Route exact path="/healthcare_finder">
             <HealthcareFinder />
+          </Route>
+          <Route exact path="/doctor_signup">
+            <DoctorSignup />
+          </Route>
+          <Route exact path="/clinic_signup">
+            <ClinicSignup />
+          </Route>
+          <Route exact path="/walk_in_clinics">
+            <WalkInClinics />
+          </Route>
+          <Route exact path="/clinic_appointments">
+            <AppointmentClinics />
+          </Route>
+          <Route exact path="/clinics/:clinic">
+            <Clinic />
+          </Route>
+          <Route exact path="/doctor_finder">
+            <FamilyDoctor />
+          </Route>
+          <Route exact path="/doctors">
+            <DoctorList />
           </Route>
         </Switch>
       </Main>
---
+++ frontend/src/pages/DoctorList.js
@@ -0,0 +1,78 @@
+import React, {useState, useEffect} from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const DoctorList = () => {
+  const [doctors, setDoctors] = useState([]);
+
+    useEffect(() => {
+      fetch(`/doctors`, {
+          method: "GET",
+        })
+          .then((res) => res.json())
+          .then((res) => setDoctors(res.data));
+    }, [])
+
+    console.log(doctors);
+
+  return (
+    <Container>
+      <MenuWrapper>
+        <MenuText>All registered doctors in Montreal:</MenuText>
+              {doctors.map((doctor) => {
+                return (
+                  <ListItem
+                    to={`/doctors/${doctor._id}`}
+                    key={doctor._id}
+                  >
+                    {doctor.firstName} {doctor.lastName}</ListItem>
+                )
+            })}
+    
+              </MenuWrapper>
+              <MapWrapper>
+                Map goes here
+              </MapWrapper>
+            </Container>
+          )
+        }
+        
+        
+    const Container = styled.div` 
+      display: flex;
+      justify-content: space-between;
+      border: 1px solid blue;
+      width: 100%;
+      height: 100%;
+    `;
+        
+    const MenuWrapper = styled.div` 
+      display: flex;
+      flex-direction: column;
+      border: 1px solid green;
+      width: 500px;
+    `;
+        
+    const MenuText = styled.p` 
+      font-size: 24px;
+      font-weight: 600;
+      padding: 0px 20px;
+    `;
+        
+    const MapWrapper = styled.div` 
+      border: 1px solid purple;
+      width: 70%;
+    `;
+        
+    const ListItem = styled(NavLink)`
+      font-size: 16px;
+      font-weight: 600;
+      padding: 0px 10px;
+      margin: 10px 20px;
+      color: black;
+      text-decoration: none;
+      cursor: pointer;
+    `;
+    
+    
+export default DoctorList;
