7d3341fc24c8f89d50ccf66f57f69629addce5ab
Joni Sadler <joni.anne.sadler@gmail.com>
2021-03-16 21:04:46 -0400

    Added doctors to individual clinic pages

--- frontend/src/pages/Clinic.js
+++ frontend/src/pages/Clinic.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const Clinic = () => {
   const [selectedClinic, setSelectedClinic] = useState({});
+  const [clinicDoctors, setClinicDoctors] = useState([]);
   const id = useParams();
   const clinic = id.clinic;
-
-  console.log(id)
-  console.log(clinic)
 
   useEffect(() => {
     fetch(`/clinics/${clinic}`, {
@@ -18,8 +16,19 @@ const Clinic = () => {
     .then((res) => {
       console.log(res)
       setSelectedClinic(res.data)
+      setClinicDoctors(res.doctorData)
     });
   }, [clinic]);
+
+  // useEffect(() => {
+  //   fetch(`/doctors`, {
+  //       method: "GET",
+  //     })
+  //       .then((res) => res.json())
+  //       .then((res) => setClinicDoctors(res.data));
+  // }, [])
+
+  console.log(clinicDoctors);
 
 
   return (
@@ -28,6 +37,17 @@ const Clinic = () => {
       <ClinicInfo>{selectedClinic.clinicAddress}</ClinicInfo>
       <ClinicInfo>{selectedClinic.hours}</ClinicInfo>
       <ClinicInfo>{selectedClinic.email}</ClinicInfo>
+      {clinicDoctors.length > 0 &&
+      <DoctorsDiv>
+        <Title>Doctors at this clinic:</Title>
+        <DoctorInfo 
+          to={`/doctors/${clinicDoctors[0]._id}`}
+          key={clinicDoctors[0]._id}
+        >
+          {clinicDoctors[0].title} {clinicDoctors[0].firstName} {clinicDoctors[0].lastName}
+        </DoctorInfo>
+      </DoctorsDiv>
+      }
     </Container>
   )
 }
@@ -48,6 +68,28 @@ const ClinicName = styled.p`
 const ClinicInfo = styled.p` 
   font-size: 24px;
   font-weight: 500;
+`;
+
+const DoctorsDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.p` 
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const DoctorInfo = styled(NavLink)` 
+  font-size: 24px;
+  font-weight: 500;
+  margin: 0px;
+  padding: 0px;
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
 `;
 
 export default Clinic;
