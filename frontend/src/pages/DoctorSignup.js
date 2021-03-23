c48aba29a9af4196b629397156275433c11fa883
Joni Sadler <joni.anne.sadler@gmail.com>
2021-03-23 19:46:36 -0400

    Added basic form validation for doctor signup

--- frontend/src/pages/DoctorSignup.js
+++ frontend/src/pages/DoctorSignup.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
-import {NavLink} from "react-router-dom";
-import styled, {keyframes} from "styled-components";
-import {fadeInDown, fadeOutUp} from "react-animations";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+import { fadeInDown, fadeOutUp } from "react-animations";
 
 const DoctorSignup = () => {
   const [signupInfo, setSignupInfo] = useState({});
@@ -11,63 +11,128 @@ const DoctorSignup = () => {
   const [secondaryClinic, setSecondaryClinic] = useState();
   const [showEmail, setShowEmail] = useState(false);
   const [showPhoneNumber, setShowPhoneNumber] = useState(false);
+  const [firstNameValidation, setFirstNameValidation] = useState(false);
+  const [lastNameValidation, setLastNameValidation] = useState(false);
+  const [emailValidation, setEmailValidation] = useState(false);
+  const [passwordValidation, setPasswordValidation] = useState(false);
 
   useEffect(() => {
     fetch(`/clinics`, {
       method: "GET",
     })
-    .then((res) => res.json())
-    .then((res) => setClinics(res.data));
-  }, [])
-
+      .then((res) => res.json())
+      .then((res) => setClinics(res.data));
+  }, []);
 
   const signupInfoHandler = (name) => {
-    return ({ target: {value} }) => {
+    return ({ target: { value } }) => {
       setSignupInfo((signupInfo) => ({ ...signupInfo, [name]: value }));
+    };
+  };
+
+  // Validate first name
+  useEffect(() => {
+    if (!signupInfo.firstName) {
+      setFirstNameValidation(false);
+    } else {
+      setFirstNameValidation(true);
     }
-  }
+    console.log(firstNameValidation);
+  }, [signupInfo.firstName]);
+
+  // Validate last name
+  useEffect(() => {
+    if (!signupInfo.lastName) {
+      setLastNameValidation(false);
+    } else {
+      setLastNameValidation(true);
+    }
+    console.log(lastNameValidation);
+  }, [signupInfo.lastName]);
+
+  // Validate email
+  useEffect(() => {
+    if (!signupInfo.email) {
+      setEmailValidation(false);
+    } else {
+      setEmailValidation(true);
+    }
+    console.log(emailValidation);
+  }, [signupInfo.email]);
+
+  // Validate password
+  useEffect(() => {
+    if (!signupInfo.password) {
+      setPasswordValidation(false);
+    } else {
+      setPasswordValidation(true);
+    }
+  }, [signupInfo.password]);
 
   const showEmailHandlerTrue = () => {
     setShowEmail(true);
-  }
+  };
 
   const showPhoneNumberHandlerTrue = () => {
     setShowPhoneNumber(true);
-  }
+  };
+
+  console.log(signupInfo);
+
+  // const handleValidation = () => {
+  //   if (signupInfo.firstName !== "") {
+  //     setFirstNameValidation(true);
+  //   } else if (signupInfo.lastName !== "") {
+  //     setLastNameValidation(true);
+  //   } else if (signupInfo.email.includes("@")) {
+  //     setEmailValidation(true);
+  //   } else if (signupInfo.password !== "") {
+  //     setPasswordValidation(true);
+  //   } else {
+  //     submitFunction();
+  //   }
+  // }
 
   const submitFunction = () => {
     setHasSubmittedInfo(true);
     fetch("/doctor_signup", {
       method: "POST",
-      body: JSON.stringify({...signupInfo, primaryClinic, secondaryClinic, showEmail, showPhoneNumber}),
-      headers: {"Accept": "application/json", "Content-type": "application/json"},
+      body: JSON.stringify({
+        ...signupInfo,
+        primaryClinic,
+        secondaryClinic,
+        showEmail,
+        showPhoneNumber,
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
     })
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-      if (res.status === 201) {
-        console.log(res.data)
-        setSignupInfo(res.data)
-      } else {
-        console.log("There is an error with the post request.")
-      }
-    })
-  }
-
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if (res.status === 201) {
+          console.log(res.data);
+          setSignupInfo(res.data);
+        } else {
+          console.log("There is an error with the post request.");
+        }
+      });
+  };
 
   return (
     <Container>
       <Title>Signup</Title>
-        <SignupWrapper>
+      <SignupWrapper>
         <Field>
           <input
             name="title"
             placeholder="Title"
             type="text"
-            required
             value={signupInfo.title}
             onChange={signupInfoHandler("title")}
-            style={{ height: "25px", width: "400px"}}
+            style={{ height: "25px", width: "400px" }}
           />
         </Field>
         <Field>
@@ -75,102 +140,103 @@ const DoctorSignup = () => {
             name="firstName"
             placeholder="First name"
             type="text"
-            required
             value={signupInfo.firstName}
             onChange={signupInfoHandler("firstName")}
-            style={{ height: "25px", width: "400px"}}
-          />
+            style={{ height: "25px", width: "400px" }}
+          />{" "}
+          *
+          {!firstNameValidation && hasSubmittedInfo && (
+            <p>This field is required</p>
+          )}
         </Field>
         <Field>
           <input
             name="lastName"
             placeholder="Last name"
             type="text"
-            required
             value={signupInfo.lastName}
             onChange={signupInfoHandler("lastName")}
-            style={{ height: "25px", width: "400px"}}
-          />
+            style={{ height: "25px", width: "400px" }}
+          />{" "}
+          *
         </Field>
         <Field>
           <input
             name="email"
             placeholder="Email"
             type="text"
-            required
             value={signupInfo.email}
             onChange={signupInfoHandler("email")}
-            style={{ height: "25px", width: "400px"}}
-          />
+            style={{ height: "25px", width: "400px" }}
+          />{" "}
+          *
         </Field>
         <Field>
           <p>Show my email as part of my public doctor profile?</p>
-          <input 
+          <input
             name="showEmail"
             type="radio"
             value="True"
             onChange={showEmailHandlerTrue}
-          /> Yes
-          <input 
-            name="showEmail"
-            type="radio"
-            value="False"
-          /> No
+          />{" "}
+          Yes
+          <input name="showEmail" type="radio" value="False" /> No
         </Field>
         <Field>
           <input
             name="phoneNumber"
             placeholder="Phone number"
             type="text"
-            required
             value={signupInfo.phoneNumber}
             onChange={signupInfoHandler("phoneNumber")}
-            style={{ height: "25px", width: "400px"}}
+            style={{ height: "25px", width: "400px" }}
           />
         </Field>
         <Field>
           <p>Show my phone number as part of my public doctor profile?</p>
-          <input 
+          <input
             name="showPhoneNumber"
             type="radio"
             value="True"
             onChange={showPhoneNumberHandlerTrue}
-          /> Yes
-          <input 
-            name="showPhoneNumber"
-            type="radio"
-            value="False"
-          /> No
+          />{" "}
+          Yes
+          <input name="showPhoneNumber" type="radio" value="False" /> No
         </Field>
         <ClinicDropdown>
           <Text>Name of the clinic you are based at:</Text>
-            <CategoryNav>
-              {clinics.map((clinic) => {
-                return (
-                  <ClinicList onClick={() => setPrimaryClinic(clinic.clinicName)}>{clinic.clinicName}</ClinicList>
-                )
-              })}
-            </CategoryNav>
-        </ClinicDropdown>        
+          <CategoryNav>
+            {clinics.map((clinic) => {
+              return (
+                <ClinicList onClick={() => setPrimaryClinic(clinic.clinicName)}>
+                  {clinic.clinicName}
+                </ClinicList>
+              );
+            })}
+          </CategoryNav>
+        </ClinicDropdown>
         <ClinicDropdown>
           <Text>If you work at an additional clinic, please select it:</Text>
-            <CategoryNav>
-              {clinics.map((clinic) => {
-                return (
-                  <ClinicList onClick={() => setSecondaryClinic(clinic.clinicName)}>{clinic.clinicName}</ClinicList>
-                ) 
-              })}
-            </CategoryNav>
+          <CategoryNav>
+            {clinics.map((clinic) => {
+              return (
+                <ClinicList
+                  onClick={() => setSecondaryClinic(clinic.clinicName)}
+                >
+                  {clinic.clinicName}
+                </ClinicList>
+              );
+            })}
+          </CategoryNav>
         </ClinicDropdown>
         <Field>
           <input
             name="specialty"
-            placeholder="Do you specialize in a particular type of medicine?"
+            placeholder="If you specialize in particular type(s) of medicine, list them. "
             type="text"
-            required
             value={signupInfo.additionalClinicName}
             onChange={signupInfoHandler("specialty")}
-            style={{ height: "25px", width: "400px"}}
+            style={{ height: "25px", width: "400px" }}
           />
         </Field>
         <Field>
@@ -178,29 +244,40 @@ const DoctorSignup = () => {
             name="password"
             placeholder="Set your password"
             type="password"
-            required
             value={signupInfo.password}
             onChange={signupInfoHandler("password")}
-            style={{ height: "25px", width: "400px"}}
-          />
+            style={{ height: "25px", width: "400px" }}
+          />{" "}
+          *
         </Field>
+        <RequiredFields>* Required fields</RequiredFields>
         <SubmitWrapper>
-          <SubmitButton onClick={submitFunction}>
-            Submit
-          </SubmitButton>
+          {firstNameValidation &&
+          lastNameValidation &&
+          emailValidation &&
+          passwordValidation ? (
+            <SubmitButton onClick={submitFunction}>Submit</SubmitButton>
+          ) : (
+            <SubmitButtonInactive>
+              Please complete all required fields
+            </SubmitButtonInactive>
+          )}
         </SubmitWrapper>
       </SignupWrapper>
-      {hasSubmittedInfo &&
-          <div>
-            <RegistrationMessaging>Thank you for registering! Your info is now securely stored in our database.</RegistrationMessaging>
-          </div>
-      }
+      {hasSubmittedInfo && (
+        <div>
+          <RegistrationMessaging>
+            Thank you for registering! Your info is now securely stored in our
+            database.
+          </RegistrationMessaging>
+        </div>
+      )}
       <LoginPrompt to={`/login`}>Return to the login page</LoginPrompt>
     </Container>
-  )
-}
+  );
+};
 
-const Container = styled.div` 
+const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -209,13 +286,13 @@ const Container = styled.div`
   width: 100%;
 `;
 
-const Title = styled.p` 
+const Title = styled.p`
   font-size: 40px;
   font-weight: 600;
   margin: 10px;
 `;
 
-const SignupWrapper = styled.div` 
+const SignupWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -224,7 +301,7 @@ const SignupWrapper = styled.div`
   padding: 20px;
 `;
 
-const Field = styled.div` 
+const Field = styled.div`
   padding: 10px;
 `;
 
@@ -246,6 +323,10 @@ const slideUp = keyframes`
   ${fadeOutUp};
 `;
 
+const RequiredFields = styled.p`
+  padding-left: 10px;
+`;
+
 const CategoryNav = styled.nav`
   background: #ffffff;
   border-radius: 3px;
@@ -264,7 +345,7 @@ const CategoryNav = styled.nav`
   /* animation: 0.3s ${slideDown}; */
 `;
 
-const ClinicList = styled.p` 
+const ClinicList = styled.p`
   margin: 5px;
   padding: 3px;
   &:hover {
@@ -273,13 +354,13 @@ const ClinicList = styled.p`
   }
 `;
 
-const SubmitWrapper = styled.div` 
+const SubmitWrapper = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 20px;
 `;
 
-const SubmitButton = styled.button` 
+const SubmitButton = styled.button`
   background-color: black;
   color: white;
   border-radius: 3px;
@@ -287,14 +368,27 @@ const SubmitButton = styled.button`
   padding: 5px;
   font-size: 30px;
   font-weight: 500;
+  cursor: pointer;
 `;
 
-const RegistrationMessaging = styled.p` 
+const SubmitButtonInactive = styled.div`
+  display: flex;
+  text-align: center;
+  background-color: grey;
+  color: white;
+  border-radius: 3px;
+  width: 300px;
+  padding: 5px;
+  font-size: 30px;
+  font-weight: 500;
+`;
+
+const RegistrationMessaging = styled.p`
   font-size: 20px;
   font-weight: 600;
 `;
 
-const LoginPrompt = styled(NavLink)` 
+const LoginPrompt = styled(NavLink)`
   font-size: 30px;
   font-weight: 600;
   margin: 50px;
