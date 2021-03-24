9d936b0094b66b05f43064c857a147cbc9e7a288
Joni Sadler <joni.anne.sadler@gmail.com>
2021-03-23 20:17:41 -0400

    Minor cleanup on Specialty page

--- frontend/src/pages/Specialty.js
+++ frontend/src/pages/Specialty.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from "react";
-import {useParams, NavLink} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const Specialty = () => {
   const specialtyObject = useParams();
-  const specialty = Object.values(specialtyObject)
+  const specialty = Object.values(specialtyObject);
   const [allDoctors, setAllDoctors] = useState([]);
   const [displayDoctorInfo, setDisplayDoctorInfo] = useState(false);
   const specialtyDoctors = [];
@@ -13,25 +13,29 @@ const Specialty = () => {
     fetch(`/specialty/${specialty}`, {
       method: "GET",
     })
-    .then((res) => res.json())
-    .then((res) => setAllDoctors(res.data));
-  }, [])
+      .then((res) => res.json())
+      .then((res) => setAllDoctors(res.data));
+  }, []);
 
   allDoctors.forEach((doctor) => {
     if (doctor.specialty === specialty.toString()) {
-      specialtyDoctors.push(doctor)
+      specialtyDoctors.push(doctor);
     }
-  })
+  });
 
-  specialtyDoctors.sort(function(a, b) {
-    if (a.lastName < b.lastName) {return -1;}
-    if (a.lastName > b.lastName) {return 1;}
+  specialtyDoctors.sort(function (a, b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    }
+    if (a.lastName > b.lastName) {
+      return 1;
+    }
     return 0;
-  })
+  });
 
   const handleDisplayInfo = () => {
     setDisplayDoctorInfo(!displayDoctorInfo);
-  }
+  };
 
   return (
     <Container>
@@ -39,59 +43,57 @@ const Specialty = () => {
       {specialtyDoctors.map((specialtyDoctor) => {
         return (
           <DoctorInfoWrapper>
-            <DoctorName
-              key={specialtyDoctor._id}
-              onClick={handleDisplayInfo}
-            >
-              {specialtyDoctor.title} {specialtyDoctor.firstName} {specialtyDoctor.lastName}     
+            <DoctorName key={specialtyDoctor._id} onClick={handleDisplayInfo}>
+              {specialtyDoctor.title} {specialtyDoctor.firstName}{" "}
+              {specialtyDoctor.lastName}
             </DoctorName>
-            {displayDoctorInfo &&
+            {displayDoctorInfo && (
               <DropdownDoctorInfo>
-                {specialtyDoctor.primaryClinic &&
-                  <DoctorInfo>Primary clinic: {specialtyDoctor.primaryClinic}</DoctorInfo>
-                }
-                {specialtyDoctor.secondaryClinic &&
-                  <DoctorInfo>Additional clinic: {specialtyDoctor.secondaryClinic}</DoctorInfo>
-                } 
-                {specialtyDoctor.phoneNumber &&
-                  <DoctorInfo>Phone number: {specialtyDoctor.phoneNumber}</DoctorInfo>
-                }
-                {specialtyDoctor.email &&
-                  <DoctorInfo>Email: {specialtyDoctor.email}</DoctorInfo>
-                }
-                <DoctorPageLink to={`/doctors/${specialtyDoctor._id}`}>View full doctor profile</DoctorPageLink>
+                {specialtyDoctor.primaryClinic && (
+                  <DoctorInfo>
+                    Primary clinic: {specialtyDoctor.primaryClinic}
+                  </DoctorInfo>
+                )}
+                {specialtyDoctor.secondaryClinic && (
+                  <DoctorInfo>
+                    Additional clinic: {specialtyDoctor.secondaryClinic}
+                  </DoctorInfo>
+                )}
+                <DoctorPageLink to={`/doctors/${specialtyDoctor._id}`}>
+                  View full doctor profile
+                </DoctorPageLink>
               </DropdownDoctorInfo>
-            }
+            )}
           </DoctorInfoWrapper>
-        )
+        );
       })}
     </Container>
-  )
-}
+  );
+};
 
-const Container = styled.div` 
+const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
   justify-content: center;
   align-items: center;
-`; 
+`;
 
-const SpecialtyTitle = styled.p` 
+const SpecialtyTitle = styled.p`
   font-size: 32px;
   font-weight: 600;
   padding: 20px;
 `;
 
-const DoctorInfoWrapper = styled.div` 
+const DoctorInfoWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
-const DoctorName = styled.p` 
+const DoctorName = styled.p`
   font-size: 22px;
   font-weight: 600;
   margin: 30px 0px 10px 0px;
@@ -100,21 +102,21 @@ const DoctorName = styled.p`
   cursor: pointer;
 `;
 
-const DropdownDoctorInfo = styled.div` 
+const DropdownDoctorInfo = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
-const DoctorInfo = styled.p` 
+const DoctorInfo = styled.p`
   font-size: 20px;
   font-weight: 500;
   margin: 0px;
   padding: 5px;
 `;
 
-const DoctorPageLink = styled(NavLink)` 
+const DoctorPageLink = styled(NavLink)`
   font-size: 20px;
   font-weight: 600;
   margin: 0px;
