87ea2f6b9da4b5f031d128d2ae75ff30949d4a49
Joni Sadler <joni.anne.sadler@gmail.com>
2021-03-27 11:57:53 -0400

    Updated homepage styling and dropdown menu

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import Header from "../components/Header";
 
 const Home = () => {
+  const [displayProviderDropdown, setDisplayProviderDropdown] = useState(false);
+
+  const handleProviderDropdown = () => {
+    setDisplayProviderDropdown(!displayProviderDropdown);
+  };
+
   return (
     <Container>
       <Header />
-
       <ContentWrapper>
         <Title>Navigating Montreal's Healthcare System</Title>
         <SubheaderText>
@@ -16,8 +21,21 @@ const Home = () => {
           things easier.
         </SubheaderText>
         <MenuDiv>
-          <Patient to={`/healthcare_finder`}>I'm seeking healthcare</Patient>
-          <Provider to={`/login`}>I'm a healthcare provider</Provider>
+          <PatientContainer>
+            <Patient to={`/healthcare_finder`}>I'm seeking healthcare</Patient>
+          </PatientContainer>
+          {/* <Provider to={`/login`}>I'm a healthcare provider</Provider> */}
+          <Provider onClick={handleProviderDropdown}>
+            <ProviderText>I'm a healthcare provider</ProviderText>
+          </Provider>
+          {displayProviderDropdown && (
+            <DropdownContainer>
+              <DropdownText to={`/login`}>Doctor Login</DropdownText>
+              <DropdownText to={`/login`}>Doctor Signup</DropdownText>
+              <DropdownText to={`/login`}>Clinic Login</DropdownText>
+              <DropdownText to={`/login`}>Clinic Signup</DropdownText>
+            </DropdownContainer>
+          )}
         </MenuDiv>
       </ContentWrapper>
     </Container>
@@ -50,8 +68,6 @@ const MenuDiv = styled.div`
 const Title = styled.p`
   font-size: 60px;
   font-weight: 700;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   margin-top: 100px;
   color: white;
   text-shadow: 1px 1px 1px #000000;
@@ -59,41 +75,79 @@ const Title = styled.p`
 
 const SubheaderText = styled.p`
   font-size: 24px;
-  font-weight: 600;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-weight: 500;
+  margin: 0px 0px 50px 0px;
   color: white;
   text-shadow: 1px 1px 1px #000000;
+`;
+
+const PatientContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  border-radius: 3px;
+  margin-bottom: 25px;
+  height: 50%;
+  width: 40%;
 `;
 
 const Patient = styled(NavLink)`
-  margin: 50px 0px 25px 25px;
+  margin: 8px 0px;
+  padding: 5px;
   text-decoration: none;
-  font-size: 36px;
+  font-size: 40px;
   font-weight: 600;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   color: white;
-  text-shadow: 1px 1px 1px #000000;
   transition: 0.2s;
+  cursor: pointer;
   &:hover {
-    font-size: 38px;
+    font-size: 42px;
   }
 `;
 
-const Provider = styled(NavLink)`
-  margin: 25px 0px 25px 25px;
+const Provider = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  border-radius: 3px;
+  margin-bottom: 25px;
+  height: 50%;
+  width: 40%;
+`;
+
+const ProviderText = styled.p`
+  margin: 8px 0px;
+  padding: 10px;
   text-decoration: none;
-  font-size: 36px;
+  font-size: 40px;
   font-weight: 600;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   color: white;
-  text-shadow: 1px 1px 1px #000000;
   transition: 0.2s;
+  cursor: pointer;
   &:hover {
-    font-size: 38px;
+    font-size: 42px;
   }
+`;
+
+const DropdownContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-left: 50px;
+  width: 15%;
+`;
+
+const DropdownText = styled(NavLink)`
+  color: white;
+  font-size: 24px;
+  font-weight: 500;
+  margin: 10px 0px;
+  padding: 0px;
+  text-decoration: none;
+  cursor: pointer;
+  text-shadow: 1px 1px 1px #000000;
 `;
 
 export default Home;
