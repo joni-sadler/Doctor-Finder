099f64083dfdf898f3059b2911e9939b5e3edc17
Joni Sadler <joni.anne.sadler@gmail.com>
2021-03-27 12:33:03 -0400

    Added styling to Doctor Login page

--- frontend/src/pages/DoctorLogin.js
+++ frontend/src/pages/DoctorLogin.js
@@ -1,12 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import {Redirect, NavLink} from "react-router-dom";
+import { Redirect, NavLink } from "react-router-dom";
 
-const HealthcareLogin = () => {
-  const [displayDoctorLogin, setDisplayDoctorLogin] = useState(false);
-  const [displayClinicLogin, setDisplayClinicLogin] = useState(false);
+const DoctorLogin = () => {
   const [doctorLoginInfo, setDoctorLoginInfo] = useState({});
-  const [clinicLoginInfo, setClinicLoginInfo] = useState({});
   const [doctorArray, setDoctorArray] = useState([]);
   const [clinicArray, setClinicArray] = useState([]);
   const [selectedDoctor, setSelectedDoctor] = useState();
@@ -14,159 +11,98 @@ const HealthcareLogin = () => {
 
   useEffect(() => {
     fetch(`/doctors`, {
-        method: "GET",
-      })
-        .then((res) => res.json())
-        .then((res) => setDoctorArray(res.data));
-  }, [])
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => setDoctorArray(res.data));
+  }, []);
 
   useEffect(() => {
     fetch(`/clinics`, {
-        method: "GET",
-      })
-        .then((res) => res.json())
-        .then((res) => setClinicArray(res.data));
-  }, [])
-
-  const displayDoctorLoginFunction = () => {
-    setDisplayDoctorLogin(!displayDoctorLogin);
-    setDisplayClinicLogin(false);
-  }
-
-  const displayClinicLoginFunction = () => {
-    setDisplayClinicLogin(!displayClinicLogin);
-    setDisplayDoctorLogin(false);
-  }
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => setClinicArray(res.data));
+  }, []);
 
   const doctorLoginHandler = (name) => {
-    return ({ target: {value} }) => {
+    return ({ target: { value } }) => {
       setDoctorLoginInfo((signupInfo) => ({ ...signupInfo, [name]: value }));
-    }
-  }
-
-
-  const clinicLoginHandler = (name) => {
-    return ({ target: {value} }) => {
-      setClinicLoginInfo((signupInfo) => ({ ...signupInfo, [name]: value }));
-    }
-  }
-
+    };
+  };
 
   const doctorSubmitFunction = () => {
     doctorArray.forEach((doctor) => {
-      if (doctorLoginInfo.email === doctor.email && doctorLoginInfo.password === doctor.password) {
+      if (
+        doctorLoginInfo.email === doctor.email &&
+        doctorLoginInfo.password === doctor.password
+      ) {
         setSelectedDoctor(doctor);
       }
-    })
-  }
-
-
-  const clinicSubmitFunction = () => {
-    clinicArray.forEach((clinic) => {
-      if (clinicLoginInfo.email === clinic.email && clinicLoginInfo.password === clinic.password) {
-        setSelectedClinic(clinic);
-        console.log(selectedClinic);
-      }
-    })
-  }
+    });
+  };
 
   return (
     <Container>
       <LoginDiv>
         <Dropdown>
-          <Title onClick={displayDoctorLoginFunction}>Doctor Login</Title>
-          {displayDoctorLogin &&
-            <LoginWrapper>
-              <Field>
-                <input
-                  name="doctorEmail"
-                  placeholder="Email"
-                  type="text"
-                  required
-                  value={doctorLoginInfo.email}
-                  onChange={doctorLoginHandler("email")}
-                  style={{ height: "25px", width: "200px"}}
-                />
-              </Field>
-              <Field>
-                <input
-                  name="password"
-                  placeholder="Password"
-                  type="password"
-                  required
-                  value={doctorLoginInfo.password}
-                  onChange={doctorLoginHandler("password")}
-                  style={{ height: "25px", width: "200px"}}
-                />
-              </Field>   
+          <Title>Doctor Login</Title>
+          <LoginWrapper>
+            <Field>
+              <input
+                name="doctorEmail"
+                placeholder="Email"
+                type="text"
+                required
+                value={doctorLoginInfo.email}
+                onChange={doctorLoginHandler("email")}
+                style={{ height: "25px", width: "200px" }}
+              />
+            </Field>
+            <Field>
+              <input
+                name="password"
+                placeholder="Password"
+                type="password"
+                required
+                value={doctorLoginInfo.password}
+                onChange={doctorLoginHandler("password")}
+                style={{ height: "25px", width: "200px" }}
+              />
+            </Field>
 
-              <SubmitWrapper>
-                <SubmitButton onClick={doctorSubmitFunction}>Submit</SubmitButton>
-              </SubmitWrapper>
+            <SubmitWrapper>
+              <SubmitButton onClick={doctorSubmitFunction}>Submit</SubmitButton>
+            </SubmitWrapper>
 
-              {selectedDoctor &&
-              <Redirect to={`/${selectedDoctor._id}/home`}/>     
-              }
-
-            </LoginWrapper>
-          }
-        </Dropdown>
-
-        <Dropdown>
-          <Title onClick={displayClinicLoginFunction}>Clinic Administrator Login</Title>
-          {displayClinicLogin &&
-            <LoginWrapper>
-              <Field>
-                <input
-                  name="clinicEmail"
-                  placeholder="Email"
-                  type="text"
-                  required
-                  value={clinicLoginInfo.email}
-                  onChange={clinicLoginHandler("email")}
-                  style={{ height: "25px", width: "200px"}}
-                />
-              </Field>
-              <Field>
-                <input
-                  name="password"
-                  placeholder="Password"
-                  type="password"
-                  required
-                  value={clinicLoginInfo.password}
-                  onChange={clinicLoginHandler("password")}
-                  style={{ height: "25px", width: "200px"}}
-                />
-              </Field>
-
-              <SubmitWrapper>
-                <SubmitButton onClick={clinicSubmitFunction}>Submit</SubmitButton>
-              </SubmitWrapper>
-
-            </LoginWrapper>
-           }
+            {selectedDoctor && (
+              <Redirect to={`/doctor_profile/${selectedDoctor._id}`} />
+            )}
+          </LoginWrapper>
         </Dropdown>
       </LoginDiv>
 
       <Signup>
         <SignupTitle>Not registered yet? </SignupTitle>
-        <SignupText to={`/doctor_signup`}>Click here to sign up as a health practitioner</SignupText>
-        <SignupText to={`/clinic_signup`}>Click here to register a clinic</SignupText>
+        <SignupText to={`/doctor_signup`}>
+          Create a new doctor account
+        </SignupText>
       </Signup>
     </Container>
-  )
-}
+  );
+};
 
-const Container = styled.div` 
+const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   height: auto;
   width: 100%;
+  background-color: #085b67;
 `;
 
-const LoginDiv = styled.div` 
+const LoginDiv = styled.div`
   display: flex;
   justify-content: space-evenly;
 `;
@@ -176,14 +112,16 @@ const Dropdown = styled.div`
   flex-direction: column;
 `;
 
-const Title = styled.p` 
-  font-size: 40px;
+const Title = styled.p`
+  font-size: 60px;
   font-weight: 600;
-  margin: 50px;
+  margin: 20px;
+  color: white;
   cursor: pointer;
+  text-shadow: 1px 1px 1px #000000;
 `;
 
-const LoginWrapper = styled.div` 
+const LoginWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -192,17 +130,17 @@ const LoginWrapper = styled.div`
   padding: 20px;
 `;
 
-const Field = styled.div` 
+const Field = styled.div`
   padding: 10px;
 `;
 
-const SubmitWrapper = styled.div` 
+const SubmitWrapper = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 20px;
 `;
 
-const SubmitButton = styled.button` 
+const SubmitButton = styled.button`
   background-color: black;
   color: white;
   border-radius: 3px;
@@ -213,7 +151,7 @@ const SubmitButton = styled.button`
   cursor: pointer;
 `;
 
-const Signup = styled.div` 
+const Signup = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -221,18 +159,23 @@ const Signup = styled.div`
   margin-top: 50px;
 `;
 
-const SignupTitle = styled.p` 
+const SignupTitle = styled.p`
   font-size: 30px;
   font-weight: 600;
+  color: white;
+  text-shadow: 1px 1px 1px #000000;
 `;
 
-const SignupText = styled(NavLink)` 
-  font-size: 18px;
+const SignupText = styled(NavLink)`
+  background-color: black;
+  padding: 10px;
+  border-radius: 3px;
+  font-size: 30px;
   font-weight: 600;
   margin: 15px;
-  color: black;
+  color: white;
   text-decoration: none;
   cursor: pointer;
 `;
 
-export default HealthcareLogin;
+export default DoctorLogin;
