9a3ba14bf0cdfe3903be6eb1a1cb5e4a0cafca89
Joni Sadler <joni.anne.sadler@gmail.com>
2021-04-03 13:28:36 -0400

    Deleted all Google Maps elements and cleaned up Map.js code

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,18 +1,9 @@
 import React, { useState, useEffect } from "react";
-// import {
-//   GoogleMap,
-//   useJsApiLoader,
-//   Marker,
-//   InfoWindow,
-// } from "@react-google-maps/api";
-
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-
 import styled from "styled-components";
 import {
   onSmallPhoneMediaQuery,
@@ -32,15 +23,8 @@ const Map = ({
   clinics,
   postalCode,
 }) => {
-  // const { isLoaded, loadError } = useJsApiLoader({
-  //   id: "google-map-script",
-  //   googleMapsApiKey: process.env.REACT_APP_MAP_KEY,
-  // });
-
   const [markers, setMarkers] = useState([]);
   const [originMarker, setOriginMarker] = useState({});
-  const [infoWindowMarker, setInfoWindowMarker] = useState(null);
-  const [showingInfoWindow, setShowingInfoWindow] = useState(false);
   const [originPoint, setOriginPoint] = useState({});
 
   const clinicCoordinates = [];
@@ -80,23 +64,7 @@ const Map = ({
     }
   }, [originPoint]);
 
-  const center = {
-    lat: 45.522944,
-    lng: -73.603893,
-  };
-
-  // if (loadError) {
-  //   console.log(loadError);
-  //   return;
-  // }
-
   const isLoaded = true;
-
-  const onClickMapHandler = (marker) => {
-    console.log(marker.clinicName);
-    setInfoWindowMarker(marker);
-    setShowingInfoWindow(true);
-  };
 
   if (clinics) {
     clinics.forEach((clinic) => {
@@ -125,51 +93,6 @@ const Map = ({
 
   return isLoaded ? (
     <MapWrapper>
-      {/* <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-        {infoWindowMarker && (
-          <InfoWindow
-            position={{
-              lat: Number(infoWindowMarker.lat),
-              lng: Number(infoWindowMarker.lng),
-            }}
-            visible={showingInfoWindow}
-            onCloseClick={() => setInfoWindowMarker(null)}
-            options={{
-              pixelOffset: new window.google.maps.Size(0, -30),
-            }}
-          >
-            <InfoWindowContainer>
-              <InfoWindowText>{infoWindowMarker.clinicName}</InfoWindowText>
-              <InfoWindowText>{infoWindowMarker.clinicAddress}</InfoWindowText>
-            </InfoWindowContainer>
-          </InfoWindow>
-        )}
-
-        {originPoint && (
-          <Marker
-            position={{
-              lat: Number(originPoint.latitude),
-              lng: Number(originPoint.longitude),
-            }}
-            icon={{
-              url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
-            }}
-          />
-        )}
-
-        {markers.map((marker) => {
-          return (
-            <div>
-              <Marker
-                position={{ lat: Number(marker.lat), lng: Number(marker.lng) }}
-                onClick={() => onClickMapHandler(marker)}
-              />
-            </div>
-          );
-        })}
-
-        <></>
-      </GoogleMap> */}
       <MapContainer center={[45.51, -73.65]} zoom={12} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
