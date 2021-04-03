b001d34245d7cd5650453398466fae61ec58cd2a
Joni Sadler <joni.anne.sadler@gmail.com>
2021-04-03 12:26:15 -0400

    Centered Montreal in new map

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -160,7 +160,7 @@ const Map = ({
 
         <></>
       </GoogleMap> */}
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[45.51, -73.65]} zoom={12} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
