e02b4a8d865d8e6427e2d35e7ad21209cf9cfc4f
Joni Sadler <joni.anne.sadler@gmail.com>
2021-04-04 13:46:29 -0400

    Basic geocoding setup

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -16,6 +16,13 @@ const containerStyle = {
   height: "100%",
 };
 
+const CACHED_POINTS = {
+  H2T3A2: { lat: 45.5260031, lng: -73.6084592 },
+  H3A2B3: { lat: 45.50773, lng: -73.57914 },
+  H2Y1C6: { lat: 45.50882, lng: -73.554138 },
+  H2S3S3: { lat: 45.53646, lng: -73.61476 },
+};
+
 const Map = ({
   acceptingPatients,
   walkInClinics,
@@ -24,7 +31,6 @@ const Map = ({
   postalCode,
 }) => {
   const [markers, setMarkers] = useState([]);
-  const [originMarker, setOriginMarker] = useState({});
   const [originPoint, setOriginPoint] = useState({});
 
   const clinicCoordinates = [];
@@ -58,13 +64,32 @@ const Map = ({
   }, [clinics]);
 
   useEffect(() => {
-    if (originPoint) {
-      setOriginMarker(originPoint);
-      return originMarker;
-    }
-  }, [originPoint]);
+    if (postalCode) {
+      console.log(CACHED_POINTS);
 
-  const isLoaded = true;
+      const cached_point = CACHED_POINTS[postalCode];
+      if (cached_point) {
+        console.log("using cached point");
+        console.log(cached_point);
+        setOriginPoint(cached_point);
+      } else {
+        console.log("network geocode request");
+        fetch(
+          `http://open.mapquestapi.com/geocoding/v1/address?key=H1yZSGy8Azm4pZPGPaBiul1tm6f9pmHK&location=${postalCode}`,
+          {
+            method: "GET",
+          }
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            const point = res.results[0].locations[0].latLng;
+            console.log(point);
+            CACHED_POINTS[postalCode] = point;
+            setOriginPoint(point);
+          });
+      }
+    }
+  }, [postalCode]);
 
   if (clinics) {
     clinics.forEach((clinic) => {
@@ -91,7 +116,7 @@ const Map = ({
 
   L.Marker.prototype.options.icon = DefaultIcon;
 
-  return isLoaded ? (
+  return (
     <MapWrapper>
       <MapContainer center={[45.51, -73.65]} zoom={12} scrollWheelZoom={false}>
         <TileLayer
@@ -102,14 +127,21 @@ const Map = ({
           return (
             <div>
               <Marker position={[Number(marker.lat), Number(marker.lng)]}>
-                <Popup>{marker.clinicName}</Popup>
+                <Popup>
+                  {marker.clinicName} <br /> {marker.clinicAddress}
+                </Popup>
               </Marker>
             </div>
           );
         })}
+        {originPoint.lat && (
+          <Marker position={[originPoint.lat, originPoint.lng]}>
+            <Popup>Your location</Popup>
+          </Marker>
+        )}
       </MapContainer>
     </MapWrapper>
-  ) : null;
+  );
 };
 
 const MapWrapper = styled.div`
@@ -125,18 +157,6 @@ const MapWrapper = styled.div`
     height: 70%;
     overflow: hidden;
   }
-`;
-
-const InfoWindowContainer = styled.div`
-  text-align: center;
-  background: white;
-  padding: 0px;
-  margin: 0px;
-`;
-
-const InfoWindowText = styled.p`
-  font-size: 12px;
-  font-weight: 400;
 `;
 
 export default Map;
